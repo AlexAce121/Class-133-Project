bc58b3e96aae7473f2d719f6f0eac2406a02908a
AlexAce121 <82585887+AlexAce121@users.noreply.github.com>
2021-11-08 16:42:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Bedroom() {
+    window.location="Bedroom.html";
+}
+
+function Bottles() {
+    window.location="Bottles.html";
+}
+
+function Clock() {
+    window.location="Clock.html";
+}
+
+function TVAC() {
+    window.location="TVAC.html";
+}
